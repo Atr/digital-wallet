8c76cfd0e51657df0b79df585c7820c1c1fda3b9
Alex Romanak <alexromanak@Alexs-MBP.attlocal.net>
2018-10-14 18:07:53 -0700

    Initial wallet home framework

--- client/components/Wallet.jsx
+++ client/components/Wallet.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
 const Wallet = props => (
-  <div>
-    <p> Hello world! </p>
+  <div className="wallet fc fd-c jc-sa ai-c">
+    <div className="title">
+      <h1> Wallet </h1>
+    </div>
+    <div className="header-bar">
+      <p> HeaderBar goes here </p>
+    </div>
+    <div className="funding-options-section">
+      <p> FundingOptions go here </p>
+    </div>
   </div>
 );
 
