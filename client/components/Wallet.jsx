f05bbeba13196a7d33140f1f30d032540a23b1aa
Alex Romanak <alexromanak@Alexs-MBP.attlocal.net>
2018-10-14 17:44:33 -0700

    React Hello World & Webpack config set up

---
+++ client/components/Wallet.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Wallet = props => (
+  <div>
+    <p> Hello world! </p>
+  </div>
+);
+
+export default Wallet;
---
+++ client/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Wallet from './components/Wallet';
+
+import './src/styles/styles.css';
+
+ReactDOM.render(<Wallet />, document.getElementById('app'));
