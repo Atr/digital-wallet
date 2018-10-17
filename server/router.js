1c69e5f617c3e902ad1f2bb88aae54cda5d04dc7
Alex Romanak <alexromanak@Alexs-MacBook-Pro.local>
2018-10-17 11:07:13 -0700

    Added get, post, put, delete routes for cards/card info

--- server/router.js
+++ server/router.js
@@ -2,6 +2,12 @@ const router = require('express').Router();
 const controller = require('./controller');
 
 router.route('/users/:id/fundingoptions')
-  .get(controller.user.getFundingOptions);
+  .get(controller.users.getFundingOptions)
+  .post(controller.users.postFundingOption);
+
+router.route('/users/:id/fundingoptions/:cardId')
+  .get(controller.users.getSingleFundingOption)
+  .put(controller.users.putSingleFundingOption)
+  .delete(controller.users.deleteSingleFundingOption);
 
 module.exports = router;
